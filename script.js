d74afe70b5b88f4ac9d4892651e1acfc7ed08d27
sanjanasajith <sanjanasajith23@gmail.com>
2024-07-29 20:53:48 +0530

    some changes made

--- script.js
+++ script.js
@@ -6,7 +6,3 @@ document.addEventListener('DOMContentLoaded', function() {
         form.reset();
     });
 });
-document.getElementById("about-link").addEventListener("click", function(event) {
-    event.preventDefault(); // Prevent default link behavior
-    window.location.href = "about.html"; // Replace "about.html" with the actual path to your About page
-  });
